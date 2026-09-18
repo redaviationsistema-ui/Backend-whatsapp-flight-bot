abd14c3bbaab0ed31bbda9f2fb975d05bef46aba
redaviationsistema-ui <redaviationsistema@gmail.com>
2026-09-18 15:19:37 -0600

    18SEP2

--- config/services.php
+++ config/services.php
@@ -36,11 +36,11 @@ return [
     ],
 
     'whatsapp' => [
-        'verify_token' => env('WHATSAPP_VERIFY_TOKEN'),
-        'phone_number_id' => env('WHATSAPP_PHONE_NUMBER_ID'),
         'business_account_id' => env('WHATSAPP_BUSINESS_ACCOUNT_ID'),
+        'phone_number_id' => env('WHATSAPP_PHONE_NUMBER_ID'),
         'access_token' => env('WHATSAPP_ACCESS_TOKEN'),
-        'graph_version' => env('WHATSAPP_API_VERSION', env('WHATSAPP_GRAPH_VERSION', 'v20.0')),
+        'verify_token' => env('WHATSAPP_VERIFY_TOKEN'),
+        'api_version' => env('WHATSAPP_API_VERSION'),
     ],
 
 ];
