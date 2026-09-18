ec7b47b694deab71dc6e30657ad38cdce75d112f
redaviationsistema-ui <redaviationsistema@gmail.com>
2026-09-18 15:22:28 -0600

    18SEP3

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'success' => true,
+        'service' => 'Sky Group WhatsApp API',
+        'status' => 'online',
+    ]);
 });
